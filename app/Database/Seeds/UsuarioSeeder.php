32e1a1d915470ac8dcdae1f5596347f31c322123
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-05-29 19:04:58 -0300

    Realizando o rollback da migração da tabela usuários

--- app/Database/Seeds/UsuarioSeeder.php
+++ app/Database/Seeds/UsuarioSeeder.php
@@ -13,6 +13,7 @@ class UsuarioSeeder extends Seeder {
         $usuario = [
             'nome' => 'Lucio Antonio',
             'email' => 'admin@admmin.com',
+            'cpf' => '215.742.780-53',
             'telefone' => '41 - 9999-9999',
         ];
 
@@ -21,6 +22,7 @@ class UsuarioSeeder extends Seeder {
         $usuario = [
             'nome' => 'Fulano de Tal',
             'email' => 'fulano@email.com',
+            'cpf' => '265.806.400-49',
             'telefone' => '41 - 8888-9999',
         ];
         $usuarioModel->protect(false)->insert($usuario);
@@ -29,4 +31,5 @@ class UsuarioSeeder extends Seeder {
     }
 
 }
+
 ?>
